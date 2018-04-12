7d7b1ff80490de1bd2132957116c96ec2b788627
artjohn1993 <artjohn1993@gmail.com>
2018-04-12 16:31:06 +0800

    add files

---
+++ app/src/main/java/co/devpartners/poolingmobileapp/SplashActivity.kt
@@ -0,0 +1,21 @@
+package co.devpartners.poolingmobileapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import org.jetbrains.anko.startActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar!!.hide()
+
+        Handler().postDelayed({
+           startActivity<MainActivity>()
+            finish()
+        },2000)
+    }
+}
